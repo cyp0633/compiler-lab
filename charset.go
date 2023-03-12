0068c7b74e1670ddfaab3e6425a9751a5d0d6285
cyp0633 <cyp0633@gmail.com>
2023-03-13 00:05:01 +0800

    charset operation

--- charset.go
+++ charset.go
@@ -4,7 +4,7 @@ package main
 type Charset struct {
 	// 字符集 ID
 	IndexID int
-	// 段 ID
+	// 段 ID。对每个字符集，段 ID 从 0 递增
 	SegmentID int
 	// 起始字符
 	FromChar rune
@@ -66,3 +66,51 @@ const (
 	// 非接受状态
 	StateUnmatch
 )
+
+// 字符集表
+var CharsetTable []*Charset
+
+// 字符的范围运算
+//
+// 生成一个字符集，包含从 fromChar 到 toChar 的所有字符
+func rangeChars(fromChar rune, toChar rune) (indexID int) {
+	var c = Charset{FromChar: fromChar, ToChar: toChar}
+	c.IndexID = CharsetTable[len(CharsetTable)-1].SegmentID + 1
+	c.SegmentID = 0
+	CharsetTable = append(CharsetTable, &c)
+	return c.IndexID
+}
+
+// 字符集的并运算
+//
+// 生成一个字符集，包含 c1 和 c2
+func unionChars(c1 rune, c2 rune) (indexID int) {
+	var cs1 = Charset{FromChar: c1, ToChar: c1}
+	cs1.IndexID = CharsetTable[len(CharsetTable)-1].SegmentID + 1
+	cs1.SegmentID = 0
+	var cs2 = Charset{FromChar: c2, ToChar: c2}
+	cs2.IndexID = CharsetTable[len(CharsetTable)-1].SegmentID + 1
+	cs2.SegmentID = 0
+	CharsetTable = append(CharsetTable, &cs1, &cs2)
+	return cs1.IndexID
+}
+
+// 字符集与字符之间的并运算
+//
+// 将原有的字符集与新字符合并
+func unionCharsetAndChar(indexID int, c rune) (newIndexID int) {
+	var cs = CharsetTable[indexID]
+	switch {
+	// c 在 cs 中，直接拷贝
+	case c >= cs.FromChar && c <= cs.ToChar:
+	// c 在 cs 正前面，合并
+	case c == cs.FromChar-1:
+		cs.FromChar = c
+	// c 在 cs 正后面，合并
+	case c == cs.ToChar+1:
+		cs.ToChar = c
+	// 俩不挨着
+	default:
+	}
+	return
+}
