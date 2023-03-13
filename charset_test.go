e230e39072b0d5c0f09cc72379a15f707fc0a48a
Yupeng Chen <cyp0633@cyp0633.icu>
2023-03-13 11:12:48 +0800

    Add unit test (not actually implemented)

---
+++ charset_test.go
@@ -0,0 +1,19 @@
+package main
+
+import "testing"
+
+func TestRangeChars(t *testing.T) {
+	rangeChars('a', 'z')
+}
+
+func TestUnionChars(t *testing.T) {
+	unionChars('a', 'z')
+}
+
+func TestUnionCharsetAndChar(t *testing.T) {
+	unionCharsetAndChar(1, 'a')
+}
+
+func TestUnionTwoCharsets(t *testing.T) {
+	unionTwoCharsets(1, 2)
+}
