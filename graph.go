6aaee705662e1f9d0960cda29fe93024e6c9d348
Yupeng Chen <cyp0633@cyp0633.icu>
2023-03-13 11:35:56 +0800

    Move graph-related data structure to graph.go

---
+++ graph.go
@@ -0,0 +1,56 @@
+package main
+
+// 图，描述 NFA 或 DFA
+type Graph struct {
+	// 图 ID
+	GraphId int
+	// 状态数
+	NumOfStates int
+	// 边的列表
+	EdgeTable []*Edge
+	// 状态列表
+	StateTable []*State
+}
+
+// 图的边
+type Edge struct {
+	// 从哪个状态出发
+	FromState int
+	// 到哪个状态
+	NextState int
+	// driver
+	DriverID int
+	// driver 类型
+	DriverType driverType
+}
+
+// 边的转换类型
+type driverType int
+
+const (
+	// epsilon 转换
+	DriverNull driverType = iota
+	// 字符
+	DriverChar
+	// 字符集
+	DriverCharset
+)
+
+// 图的状态（节点）
+type State struct {
+	// 状态 ID
+	StateID int
+	// 状态类型
+	StateType stateType
+	// 类型
+	Category LexemeCategory
+}
+
+type stateType int
+
+const (
+	// 接受状态
+	StateMatch stateType = iota
+	// 非接受状态
+	StateUnmatch
+)
