0e2d6c8762c3f222b90f1d5d27f3e0b176fd22ec
Yupeng Chen <cyp0633@cyp0633.icu>
2023-04-03 11:02:12 +0800

    Closure of item set

--- lab2/lr0.go
+++ lab2/lr0.go
@@ -92,3 +92,70 @@ type ProductionInfo struct {
 
 // 产生式概述表
 var ProductionInfoTable []*ProductionInfo
+
+// 项目集的 Closure 函数
+func (itemSet *ItemSet) Closure() (closureSet *ItemSet) {
+	// 加入 I 中已有的项目
+	closureSet = copyItemSet(itemSet)
+	// 循环直到 closureSet 不再增大
+	lastSize := 0
+	for len(closureSet.ItemTable) != lastSize {
+		lastSize = len(closureSet.ItemTable)
+		// 遍历 closureSet 中的每个项目
+		for _, item := range closureSet.ItemTable {
+			// 寻找 \alpha \cdot B \beta
+			// 即 dotPosition 处之后为非终结符的项目
+			B := item.Production.BodySymbol[item.DotPosition]
+			B, ok := B.(*NonTerminalSymbol)
+			if !ok {
+				continue
+			}
+			// 重新遍历 I
+			for _, item1 := range itemSet.ItemTable {
+				// 寻找 B -> \cdot \gamma
+				if item1.NonTerminalSymbol != B || item1.DotPosition != 0 {
+					continue
+				}
+				// 将 B -> \cdot \gamma 加入 closureSet
+				item2 := &LR0Item{
+					NonTerminalSymbol: item1.NonTerminalSymbol,
+					Production:        item1.Production,
+					DotPosition:       item1.DotPosition,
+					Type:              item1.Type,
+				}
+				closureSet.ItemTable = append(closureSet.ItemTable, item2)
+			}
+		}
+	}
+	return
+}
+
+// 深拷贝项目集
+func copyItemSet(itemSet *ItemSet) (newItemSet *ItemSet) {
+	newItemSet = &ItemSet{
+		ID: maxItemSetID() + 1,
+	}
+	for _, item := range itemSet.ItemTable {
+		// 深拷贝时不需要继续拷贝到 NonTerminal 和 Production
+		// 因为语法分析时没必要改变这些东西
+		item1 := &LR0Item{
+			NonTerminalSymbol: item.NonTerminalSymbol,
+			Production:        item.Production,
+			DotPosition:       item.DotPosition,
+			Type:              item.Type,
+		}
+		newItemSet.ItemTable = append(newItemSet.ItemTable, item1)
+	}
+	// 加入项目集表
+	ItemSetTable = append(ItemSetTable, newItemSet)
+	return
+}
+
+// 项目集表的最大 ID
+func maxItemSetID() (maxID int) {
+	if len(ItemSetTable) == 0 {
+		return -1
+	}
+	maxID = ItemSetTable[len(ItemSetTable)-1].ID
+	return
+}
